2ebb2d6a51fff6f8189610753c46ead70185c070
Stella and Charlie <147658063+teamcons@users.noreply.github.com>
2024-09-24 20:51:10 +0200

    Update backup-thingie.sh

--- backup-thingie.sh
+++ backup-thingie.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+# Where to save everything
 WHERE="/media/teamcons/HDD Rin/"
 cd "$WHERE"
 
+# WHere to save HOME
 BACKUPFOLDER="./slashhomeslashteamcons"
 LOG="./backup-rsync.log"
 LOG2="./backup-rsync-previous.log"
 
+# Where to save System
 BACKUPIMAGE="./system-backup.img.gz"
 
 
 
-
-
+######## HOME BACKUP ########
 
 # If the log is older than 3 days
 # Could do without an IF, but theres no point in agressively backing up every smol change
@@ -22,8 +24,6 @@ if [[ ! $(find "$LOG" -mtime -3 >> /dev/null) ]] ; then
 
         # Keep previous log
         cat "$LOG" > "$LOG2"
-
-
 
         # Lets goooo
         rsync -vir --delete --exclude=".*" \
@@ -40,20 +40,18 @@ fi
 
 
 
-##########
+######## SYSTEM BACKUP ########
 
-#SYSTEM
 
 # If there is no file modified under 30 days to be found
 if [[ ! $(find "$BACKUPIMAGE" -mtime -30 >> /dev/null) ]] ; then
 
                 # Save to specified place a gzipped backup
-                # FIXME: Graphical prompt
-                sudo bash -c     "dd if=/nvme0n1p1 bs=32m | gzip -c > $BACKUPIMAGE"
+                pkexec bash -c     "dd if=/nvme0n1p1 bs=32m | gzip -c > $BACKUPIMAGE"
 
 
                 # Nice notif
-                notify-send “Stellas backup thingie” „Backed up“ --icon face-cool
+                notify-send "Stellas backup thingie" "Backed up" --icon face-cool
 fi
 
 
